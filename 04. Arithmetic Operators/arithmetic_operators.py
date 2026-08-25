bb386107f5febc960e89ddbe4ed36aee3fd49683
DivyanshSaharan519 <divyansh.saharan.cg@gmail.com>
2026-08-25 17:15:42 +0530

    file added

---
+++ 04. Arithmetic Operators/arithmetic_operators.py
@@ -0,0 +1,12 @@
+# Task 1 - Basic Arithmetic
+
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
