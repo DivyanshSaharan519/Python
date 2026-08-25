969f8c7655747724c3cb827afa6785b771882b9b
DivyanshSaharan519 <divyansh.saharan.cg@gmail.com>
2026-08-25 17:18:24 +0530

    file added

---
+++ 04. Arithmetic Operators/integer_and-float.py
@@ -0,0 +1,25 @@
+integer_number = 10
+float_number = 2.5
+
+print("\n--- Task 2: Integer and Float ---")
+
+result = integer_number + float_number
+print("Addition:", result, type(result))
+
+result = integer_number - float_number
+print("Subtraction:", result, type(result))
+
+result = integer_number * float_number
+print("Multiplication:", result, type(result))
+
+result = integer_number / float_number
+print("Division:", result, type(result))
+
+result = integer_number // float_number
+print("Floor Division:", result, type(result))
+
+result = integer_number % float_number
+print("Modulus:", result, type(result))
+
+result = integer_number ** float_number
+print("Exponentiation:", result, type(result))
---
+++ 04. Arithmetic Operators/student_marks.py
@@ -0,0 +1,10 @@
+maths_marks = 80
+science_marks = 75
+english_marks = 85
+
+total_marks = maths_marks + science_marks + english_marks
+average_marks = total_marks / 3
+
+print("\n--- Task 3: Student Marks ---")
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
