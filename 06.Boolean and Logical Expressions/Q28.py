5c4c9326658644d6706d463eb1222c0a79a487e4
DivyanshSaharan519 <divyansh.saharan.cg@gmail.com>
2026-09-04 17:37:13 +0530

    file added

---
+++ 06.Boolean and Logical Expressions/Q28.py
@@ -0,0 +1,4 @@
+values = [0, 1, -5, "", "Python", False, True, None]
+
+for value in values:
+    print(value, "->", bool(value))
---
+++ 06.Boolean and Logical Expressions/Q30.py
@@ -0,0 +1,7 @@
+values = [0, 1, "", "Python", False, None]
+
+for value in values:
+    print("Value:", value)
+    print("Data Type:", type(value))
+    print("Boolean:", bool(value))
+    print()
