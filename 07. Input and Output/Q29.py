b333310d18f055a03bced7ee5be61e402c0bace1
DivyanshSaharan519 <divyansh.saharan.cg@gmail.com>
2026-09-04 17:43:06 +0530

    file added

---
+++ 07. Input and Output/Q29.py
@@ -0,0 +1,7 @@
+name = input("Enter student name: ")
+age = int(input("Enter age: "))
+marks = float(input("Enter marks: "))
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Marks: {marks}")
